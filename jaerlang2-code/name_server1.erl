4082b3d9cc6ab70e8d3781bee4765516d96f189b
why520crazy <why520crazy@163.com>
2015-06-17 14:34:00 +0800

    change readme file

--- jaerlang2-code/name_server1.erl
+++ jaerlang2-code/name_server1.erl
@@ -7,15 +7,17 @@
 %%  Visit http://www.pragmaticprogrammer.com/titles/jaerlang2 for more book information.
 %%---
 -module(name_server1).
--export([init/0, add/2, find/1, handle/2]).
+-export([init/0, add/2, find/1, handle/2,allNames/0]).
 -import(server3, [rpc/2]).
 
 %% client routines
 add(Name, Place) -> rpc(name_server, {add, Name, Place}).
 find(Name)       -> rpc(name_server, {find, Name}).
+allNames()       -> rpc(name_server, {allKeys}).
 
 %% callback routines
 init() -> dict:new().
 
 handle({add, Name, Place}, Dict) -> {ok, dict:store(Name, Place, Dict)};
-handle({find, Name}, Dict)       -> {dict:find(Name, Dict), Dict}.
+handle({find, Name}, Dict)       -> {dict:find(Name, Dict), Dict};
+handle({allKeys}, Dict)       -> {dict:fetch_keys(Dict), Dict}.
